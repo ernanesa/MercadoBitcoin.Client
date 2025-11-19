12f3978096c605efebaed126778901a5155c5c63
Ernane Sa <ernane.sa@gmail.com>
2025-11-19 10:48:33 -0300

    refactor: improve variable naming for clarity and consistency across multiple files

--- src/MercadoBitcoin.Client/Internal/Helpers/JsonHelper.cs
+++ src/MercadoBitcoin.Client/Internal/Helpers/JsonHelper.cs
@@ -30,10 +30,10 @@ namespace MercadoBitcoin.Client.Internal.Helpers
             return (T?)JsonSerializer.Deserialize(json, typeInfo);
         }
 
-        public static async Task<T?> DeserializeAsync<T>(Stream utf8Json, CancellationToken ct)
+        public static async Task<T?> DeserializeAsync<T>(Stream utf8Json, CancellationToken cancellationToken)
         {
             var typeInfo = GetTypeInfo<T>() ?? throw new NotSupportedException($"Type not registered for source-gen JSON: {typeof(T).FullName}. Add [JsonSerializable] to context.");
-            return await JsonSerializer.DeserializeAsync(utf8Json, typeInfo, ct).ConfigureAwait(false);
+            return await JsonSerializer.DeserializeAsync(utf8Json, typeInfo, cancellationToken).ConfigureAwait(false);
         }
 
         private static System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>? GetTypeInfo<T>()
@@ -41,43 +41,43 @@ namespace MercadoBitcoin.Client.Internal.Helpers
             // Main map: add new DTOs if necessary
             return typeof(T) switch
             {
-                var t when t == typeof(ErrorResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.ErrorResponse,
-                var t when t == typeof(AccountResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AccountResponse,
-                var t when t == typeof(AuthorizeRequest) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AuthorizeRequest,
-                var t when t == typeof(AuthorizeResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AuthorizeResponse,
-                var t when t == typeof(CancelOpenOrdersResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.CancelOpenOrdersResponse,
-                var t when t == typeof(GetTierResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.GetTierResponse,
-                var t when t == typeof(GetMarketFeesResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.GetMarketFeesResponse,
-                var t when t == typeof(CryptoBalanceResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.CryptoBalanceResponse,
-                var t when t == typeof(PlaceOrderRequest) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.PlaceOrderRequest,
-                var t when t == typeof(PlaceOrderResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.PlaceOrderResponse,
-                var t when t == typeof(CancelOrderResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.CancelOrderResponse,
-                var t when t == typeof(OrderBookResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.OrderBookResponse,
-                var t when t == typeof(TickerResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TickerResponse,
-                var t when t == typeof(TickerResponse[]) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TickerResponseArray,
-                var t when t == typeof(TradeResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TradeResponse,
-                var t when t == typeof(TradeResponse[]) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TradeResponseArray,
-                var t when t == typeof(ListCandlesResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.ListCandlesResponse,
-                var t when t == typeof(ListSymbolInfoResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.ListSymbolInfoResponse,
-                var t when t == typeof(PositionResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.PositionResponse,
-                var t when t == typeof(OrderResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.OrderResponse,
+                var type when type == typeof(ErrorResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.ErrorResponse,
+                var type when type == typeof(AccountResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AccountResponse,
+                var type when type == typeof(AuthorizeRequest) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AuthorizeRequest,
+                var type when type == typeof(AuthorizeResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AuthorizeResponse,
+                var type when type == typeof(CancelOpenOrdersResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.CancelOpenOrdersResponse,
+                var type when type == typeof(GetTierResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.GetTierResponse,
+                var type when type == typeof(GetMarketFeesResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.GetMarketFeesResponse,
+                var type when type == typeof(CryptoBalanceResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.CryptoBalanceResponse,
+                var type when type == typeof(PlaceOrderRequest) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.PlaceOrderRequest,
+                var type when type == typeof(PlaceOrderResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.PlaceOrderResponse,
+                var type when type == typeof(CancelOrderResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.CancelOrderResponse,
+                var type when type == typeof(OrderBookResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.OrderBookResponse,
+                var type when type == typeof(TickerResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TickerResponse,
+                var type when type == typeof(TickerResponse[]) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TickerResponseArray,
+                var type when type == typeof(TradeResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TradeResponse,
+                var type when type == typeof(TradeResponse[]) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.TradeResponseArray,
+                var type when type == typeof(ListCandlesResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.ListCandlesResponse,
+                var type when type == typeof(ListSymbolInfoResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.ListSymbolInfoResponse,
+                var type when type == typeof(PositionResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.PositionResponse,
+                var type when type == typeof(OrderResponse) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.OrderResponse,
                 // Collection of OrderResponse: use fallback (or add manually when necessary)
-                var t when t == typeof(AssetFee) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AssetFee,
-                var t when t == typeof(Deposit) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Deposit,
-                var t when t == typeof(Network) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Network,
-                var t when t == typeof(FiatDeposit) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.FiatDeposit,
-                var t when t == typeof(DepositAddresses) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.DepositAddresses,
-                var t when t == typeof(Addresses) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Addresses,
-                var t when t == typeof(Response) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Response,
-                var t when t == typeof(Source) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Source,
-                var t when t == typeof(Config) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Config,
-                var t when t == typeof(Extra) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Extra,
-                var t when t == typeof(Qrcode) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Qrcode,
-                var t when t == typeof(Withdraw) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Withdraw,
-                var t when t == typeof(WithdrawCoinRequest) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.WithdrawCoinRequest,
-                var t when t == typeof(BankAccount) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.BankAccount,
-                var t when t == typeof(Fees) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Fees,
-                var t when t == typeof(BRLWithdrawConfig) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.BRLWithdrawConfig,
+                var type when type == typeof(AssetFee) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.AssetFee,
+                var type when type == typeof(Deposit) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Deposit,
+                var type when type == typeof(Network) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Network,
+                var type when type == typeof(FiatDeposit) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.FiatDeposit,
+                var type when type == typeof(DepositAddresses) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.DepositAddresses,
+                var type when type == typeof(Addresses) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Addresses,
+                var type when type == typeof(Response) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Response,
+                var type when type == typeof(Source) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Source,
+                var type when type == typeof(Config) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Config,
+                var type when type == typeof(Extra) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Extra,
+                var type when type == typeof(Qrcode) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Qrcode,
+                var type when type == typeof(Withdraw) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Withdraw,
+                var type when type == typeof(WithdrawCoinRequest) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.WithdrawCoinRequest,
+                var type when type == typeof(BankAccount) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.BankAccount,
+                var type when type == typeof(Fees) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.Fees,
+                var type when type == typeof(BRLWithdrawConfig) => (System.Text.Json.Serialization.Metadata.JsonTypeInfo<T>?)(object)MercadoBitcoinJsonSerializerContext.Default.BRLWithdrawConfig,
                 _ => null
             };
         }
